580462d7c114a77e4912e042425c9e3223f97109
Pedro Henrique de Oliveira Monteiro <ph817285@gmail.com>
2025-01-26 21:24:09 -0300

    feat: Sistema de Rotas Simples
    -Implementa funções com requisições para criar as rotas

---
+++ routes-systems/routes/router.php
@@ -0,0 +1,11 @@
+<?php
+
+$routes = [
+    'GET' => [
+        '/' => 'HomeController',
+        '/contact' => 'ContatoController',
+    ],
+    'POST' => [
+        '/contact' => 'ContatoController',
+    ],
+];
