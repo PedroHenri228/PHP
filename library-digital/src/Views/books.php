e38b6a673ef94bd9472b32ad939c5db901bcab3c
Pedro Henrique de Oliveira Monteiro <ph817285@gmail.com>
2025-02-01 16:43:05 -0300

    feat: Adicionando os design

--- library-digital/src/Views/books.php
+++ library-digital/src/Views/books.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books</title>
+    <link rel="stylesheet" href="src/public/sass/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:wght@300;400;500;600&family=Sora:wght@100..800&display=swap"
+        rel="stylesheet">
 </head>
+
 <body>
-    
+
     <?php if (!empty($results['results'])): ?>
         <ul>
             <?php foreach ($results['results'] as $book): ?>
-                <?php $bookDetails = $book['book_details'][0]?>
-                <?php $isbn = $book['isbns'][0]?>
+                <?php $bookDetails = $book['book_details'][0] ?>
+                <?php $isbn = $book['isbns'][0] ?>
                 <li>
                     <strong>Título:</strong> <?= htmlspecialchars($bookDetails['title'] ?? 'Sem título') ?><br>
                     <strong>Autor:</strong> <?= htmlspecialchars($bookDetails['author'] ?? 'Desconhecido') ?><br>
                     <strong>Descrição:</strong> <?= htmlspecialchars($bookDetails['description'] ?? 'Desconhecido') ?><br>
-                    <a href="http://localhost:8080/library-digital/books/<?= htmlspecialchars($isbn['isbn13']) ?> ">Ver Livro</a>
+                    <a href="http://localhost:8080/library-digital/books/<?= htmlspecialchars($isbn['isbn13']) ?> ">Ver
+                        Livro</a>
                 </li>
             <?php endforeach; ?>
         </ul>
@@ -24,4 +33,5 @@
         <p>Nenhum livro encontrado.</p>
     <?php endif; ?>
 </body>
+
 </html>
